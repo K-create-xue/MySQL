d0d451adf64c163e032cf543a91cc497c85e3346
K-create-xue <76169472+K-create-xue@users.noreply.github.com>
2021-10-21 14:18:49 +0800

    mysql接口学习

--- mysql_test.c
+++ mysql_test.c
@@ -1,4 +1,4 @@
-  /*************************************************************************
+  /*************************************************************************                                                                                                                                                                                                                                                                                                                                                                                                                 
       > File Name: mysql_test.c
       > Author: Black_K
       > Mail: xzp01@foxmail.com 
@@ -18,7 +18,7 @@ W>int main(int argc,char* argv[]){
        if(mysql==NULL){
            printf("mysql init error\n");
            return -1;
-       }                                                                                                                                                                                                            
+       }
       //mysql_real_connect(句柄，服务器IP，用户名，密码，数据库名称
       //，端口号，套接字文件，客户端标志)；
       if(mysql_real_connect(mysql,"172.22.226.182","root","1","dbname",
@@ -31,12 +31,41 @@ W>int main(int argc,char* argv[]){
           mysql_close(mysql);
           return -1;
       }
-W>    char *insert="insert tbname values(NULL,'宋江',1,99.66,now());";
+      //插入
+     /* char *insert="insert tbname values(NULL,'史进',1,99.66,now());";
       if(mysql_query(mysql,insert)!=0){
-          printf("query failed: %s \n",mysql_error(mysql));
+          printf("query failed: %s \n",mysql_error(mysql));   
           return -1;
       }
+      */
+      //更新
+      /*char* update="update tbname set score=66.99 where name='宋江';";
+      if(mysql_query(mysql,update)!=0){
+        printf("update failed : %s \n",mysql_error(mysql));
+        return -1;
+      }
+      */
+W>    char* select="select * from tbname;";
+      if(mysql_query(mysql,select)!=0){
+        printf("query error : %s \n",mysql_error(mysql));
+        return -1;
+      }
+      MYSQL_RES* res=mysql_store_result( mysql);
+      if(res==NULL){
+        printf("store result failed: %s \n",mysql_error(mysql));
+        return -1;
+      }
+      int num_row=mysql_num_rows(res);
+      printf("id\tname\tsex\tcore\tbirth\n");
+      for(int i=0;i<num_row;++i){
+        MYSQL_ROW row=mysql_fetch_row(res);
+        if(row[2]==0)//错误判断
+            printf("%s\t%s\t0\t%s\t%s\t\n",row[0],row[1],row[3],row[4]);
+        else
+            printf("%s\t%s\t1\t%s\t%s\t\n",row[0],row[1],row[3],row[4]);
   
+      }
+      mysql_free_result(res);
       mysql_close(mysql);
       return 0;
   }
